89bddeb63d2c08d5b52c9c1822bce56cb751c276
Honza Mach <honza.mach.ml@gmail.com>
2019-10-08 17:53:14 +0200

    Fix: Added the whole msms_metadata subdirectory to ignore list for generating documentation.

--- conf.py
+++ conf.py
@@ -78,7 +78,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['doc/_build', 'Thumbs.db', '.DS_Store', 'msms_metadata/roles']
+exclude_patterns = ['doc/_build', 'Thumbs.db', '.DS_Store', 'msms_metadata']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
